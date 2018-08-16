d8740a64c2ddbfd3332f6ae43cc163a6e0c4ad81
acidDrain <jmgrow@gmail.com>
2018-08-15 21:15:32 -0500

    Added get_istio script and updated requirements

---
+++ env-kube-lab_setup.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/env bash
+export NAME=kube-lab.k8s.local
+export KOPS_STATE_STORE=s3://kube-lab.k8s.local-state-store
+
+export PATH="$PATH:/Users/jamesgrow/projects/kube-lab.k8s.local/istio-1.0.0/bin"
